a3e518bbe7597f62c5c1fff57b9e64ecd10b0257
Aaron B <abriscoe2008@gmail.com>
2019-05-25 22:51:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,12 +14,20 @@ def long_planeteer_calls(array)
   end
 end
 
+
+
 def find_the_cheese(array)
   cheeses = %w[gouda cheddar camembert]
-array.find do |cheese?|
-    cheeses.include?(cheese?)
+
+  array.find do |maybe_cheese|
+    cheeses.include?(maybe_cheese)
   end
 end
+find_the_cheese(array)
+
+
+
+
 
 
 
